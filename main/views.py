608197c391e7527f8fec9e74f68aed81f357e643
Stepan Gutsol <gucol2004@mail.ru>
2024-06-16 01:30:42 +0500

    Concordance function test

--- main/views.py
+++ main/views.py
@@ -1,45 +1,59 @@
 import os
-import textblob as tb
+import nltk
+import textblob
 from django.shortcuts import render
 from textblob import TextBlob
 from collections import Counter
 from .models import Main
 import nltk
-import pymorphy3
+from nltk.corpus import gutenberg, PlaintextCorpusReader
+from nltk.text import Text
 # nltk.download('wordnet')
 # nltk.download('punkt')
+# nltk.download('gutenberg')
 
 # Create your views here.
 
 url = 'C:/Users/Admin/HSE_CORPORA/main/static/PECLAP'
 info = ''
 raw_info = []
-
-for filename in os.listdir(url):
-    folder_url = os.path.join(url, filename)
-    count = 0
-    if filename == 'BI_PE':
-        count = 58
-    elif filename == 'E':
-        count = 68
-    elif filename == 'HIST':
-        count = 43
-    elif filename == 'LAW':
-        count = 77
-    elif filename == 'M':
-        count = 58
-    elif filename == 'POLIT':
-        count = 29
-    for i in range(1, count + 1):
-        file = os.path.join(folder_url, filename)
-        name = file + ' (' + str(i) + ').txt'
-        # print(name)
-        with open(name, 'r') as f:
-            lines = f.readlines()
-            # print(lines)
-            lines = ' '.join(lines)
-            raw_info.append(lines)
-            info += lines
+folders = ''
+# for filename in os.listdir(url):
+#     folder_url = os.path.join(url, filename)
+#     count = 0
+#     if filename == 'BI_PE':
+#         count = 58
+#     elif filename == 'E':
+#         count = 68
+#     elif filename == 'HIST':
+#         count = 43
+#     elif filename == 'LAW':
+#         count = 77
+#     elif filename == 'M':
+#         count = 58
+#     elif filename == 'POLIT':
+#         count = 29
+#     corpus = gutenberg.words()
+#     for i in range(1, count + 1):
+        # file = os.path.join(folder_url, filename)
+        # name = file + ' (' + str(i) + ').txt'
+        # # print(name)
+        # with open(name, 'r') as f:
+        #     lines = f.readlines()
+        #     # print(lines)
+        #     lines = ' '.join(lines)
+        #     raw_info.append(lines)
+        #     info += lines
+        # corpus += gutenberg.words(name)
+wordlists = PlaintextCorpusReader(url, '.*', encoding='cp1251')
+# print(wordlists.raw())
+words = wordlists.words(fileids=wordlists.fileids())
+count = 0
+# for word in words:
+#     count+=1
+#     print(word, count)
+raw_text = wordlists.raw()
+# print(corpus_view)
 info = info.lower()
 tokens = 676301
 proportion = 1000000/676301
@@ -96,6 +110,10 @@ def home(request):
     #     count = 0
     return render(request, 'home.html', context={'current_info': current_info})
 
-def peclap(request):
+def peclap(request): # Concordance
     current_info = Main.objects.all()
+    # print(wordlists.raw())
+    text = Text(list(raw_text.split(' ')))
+    print(text)
+    current_info = text.concordance_list('the', lines=100000)
     return render(request, 'peclap.html', context={'current_info': current_info})
